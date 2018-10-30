a5f4541a733631c7c72872bb18d2fb42fb7615c6
Abel Weldaregay <aweld002@odu.edu>
2018-10-30 10:49:52 -0400

    update database dump

--- milestone2dump.sql
+++ milestone2dump.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 30, 2018 at 06:54 AM
+-- Generation Time: Oct 30, 2018 at 10:47 AM
 -- Server version: 10.0.36-MariaDB-1~trusty
 -- PHP Version: 5.5.9-1ubuntu4.26
 
@@ -23,62 +23,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `comments`
---
-
-CREATE TABLE IF NOT EXISTS `comments` (
-  `commentId` int(25) NOT NULL,
-  `parent_messageId` int(25) NOT NULL,
-  `comment` text NOT NULL,
-  `commentUserId` int(25) NOT NULL,
-  `timeOfComent` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=462 DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `groupInvite`
---
-
-CREATE TABLE IF NOT EXISTS `groupInvite` (
-  `groupId` int(20) NOT NULL,
-  `inviteId` int(11) NOT NULL,
-  `userIdInvited` int(20) NOT NULL,
-  `timeOfInvite` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `groups`
---
-
-CREATE TABLE IF NOT EXISTS `groups` (
-  `groupId` int(11) NOT NULL,
-  `groupName` varchar(244) NOT NULL,
-  `ownerUserId` int(11) NOT NULL,
-  `type` varchar(50) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=72 DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `posts`
---
-
-CREATE TABLE IF NOT EXISTS `posts` (
-  `messageId` int(20) NOT NULL,
-  `groupId` int(20) NOT NULL,
-  `message` text NOT NULL,
-  `UserId` int(20) NOT NULL,
-  `TimeOfPost` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `likeCount` int(22) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=555 DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `user`
 --
 
@@ -93,60 +37,22 @@ CREATE TABLE IF NOT EXISTS `user` (
   `Password` varchar(244) NOT NULL,
   `PhoneNumber` text NOT NULL,
   `profile` varchar(344) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `userGroup`
---
-
-CREATE TABLE IF NOT EXISTS `userGroup` (
-  `id` int(11) NOT NULL,
-  `groupUserId` int(11) NOT NULL,
-  `groupId` int(11) NOT NULL,
-  `joinedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `userLikes`
+-- Dumping data for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `userLikes` (
-  `message_id` int(20) NOT NULL,
-  `userId` int(20) NOT NULL,
-  `reactionId` int(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `user` (`ID`, `UserName`, `FirstName`, `LastName`, `Email`, `Status`, `ProfilePicture`, `Password`, `PhoneNumber`, `profile`) VALUES
+(45, 'mater@rsprings.gov', 'Tom', 'Mater', 'mater@rsprings.gov', '', '', '@mater', '', ''),
+(46, 'porsche@rsprings.gov', 'Sally', 'Carrera', 'porsche@rsprings.gov', '', '', '@sally', '', ''),
+(47, 'hornet@rsprings.gov', 'Doc', 'Hudson', 'hornet@rsprings.gov', '', '', '@doc', '', ''),
+(48, 'topsecret@agent.org', 'Finn', 'McMissile', 'topsecret@agent.org', '', '', '@mcmissile', '', ''),
+(49, 'kachow@rusteze.com', 'Lightning', 'McQueen', 'kachow@rusteze.com', '', '', '@mcqueen', '', '');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `comments`
---
-ALTER TABLE `comments`
-  ADD PRIMARY KEY (`commentId`);
-
---
--- Indexes for table `groupInvite`
---
-ALTER TABLE `groupInvite`
-  ADD PRIMARY KEY (`inviteId`);
-
---
--- Indexes for table `groups`
---
-ALTER TABLE `groups`
-  ADD PRIMARY KEY (`groupId`);
-
---
--- Indexes for table `posts`
---
-ALTER TABLE `posts`
-  ADD PRIMARY KEY (`messageId`);
 
 --
 -- Indexes for table `user`
@@ -155,51 +61,14 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indexes for table `userGroup`
---
-ALTER TABLE `userGroup`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `userLikes`
---
-ALTER TABLE `userLikes`
-  ADD UNIQUE KEY `message_id` (`message_id`,`userId`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `comments`
---
-ALTER TABLE `comments`
-  MODIFY `commentId` int(25) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=462;
---
--- AUTO_INCREMENT for table `groupInvite`
---
-ALTER TABLE `groupInvite`
-  MODIFY `inviteId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
---
--- AUTO_INCREMENT for table `groups`
---
-ALTER TABLE `groups`
-  MODIFY `groupId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=72;
---
--- AUTO_INCREMENT for table `posts`
---
-ALTER TABLE `posts`
-  MODIFY `messageId` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=555;
---
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=45;
---
--- AUTO_INCREMENT for table `userGroup`
---
-ALTER TABLE `userGroup`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=50;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=50;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
