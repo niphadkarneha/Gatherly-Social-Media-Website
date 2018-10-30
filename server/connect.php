6ea647b4028c7b540556fa93b00cf7094a6ccefd
Abel Weldaregay <aweld002@odu.edu>
2018-10-30 12:57:02 -0400

    Update connect.php

--- server/connect.php
+++ server/connect.php
@@ -3,10 +3,10 @@
 
 	    // Database details
 
-	    private $host = "localhost";
+	    private $host = "handson-mysql";
 	    private $db_name = "fordFanatics";
-	    private $username = "admin";
-	    private $password = "monarchs";
+	    private $username = "kumar";
+	    private $password = "kumar";
 
 	    public function getConnection(){
 
