42ce46da6277b406ba8f54ca586c56a2027a3438
Abel Weldaregay <aweld002@odu.edu>
2018-11-27 16:05:59 -0500

    update favicon path

--- mainpage.php
+++ mainpage.php
@@ -63,7 +63,7 @@ if(!isset($_SESSION['UserId']))
 <html>
 <title>Gatherly</title>
 <head>
-  <link rel="shortcut icon" type="image/jpg" href="icons/g.jpg">
+  <link rel="shortcut icon" type="image/jpg" href="g.jpg">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
